af85a3cbff59e1cafb2e0c436cbe16e499146e24
Angie Gonzalez <angiem.go@gmail.com>
2023-03-03 00:54:02 -0600

    made all API calls

---
+++ api/mergedData.js
@@ -0,0 +1,28 @@
+import { deleteSingleSong, getSnippetsBySong, getSingleSong } from './songData';
+import { deleteSingleSnippet, getSingleSnippet } from './snippetData';
+
+const viewSongDetails = (firebaseKey) => new Promise((resolve, reject) => {
+  getSingleSong(firebaseKey).then((song) => {
+    getSnippetsBySong(song.firebaseKey)
+      .then((songSnippets) => resolve({ ...song, songSnippets }));
+  }).catch(reject);
+});
+
+const viewSnippetDetails = (firebaseKey) => new Promise((resolve, reject) => {
+  getSingleSnippet(firebaseKey).then((snippet) => {
+    getSingleSong(snippet.song_id)
+      .then((snippetData) => resolve({ ...snippet, snippetData }));
+  }).catch(reject);
+});
+
+const deleteSongSnippets = (songId) => new Promise((resolve, reject) => {
+  getSnippetsBySong(songId).then((snippetsArray) => {
+    const deleteSnippetPromises = snippetsArray.map((snippet) => deleteSingleSnippet(snippet.firebaseKey));
+
+    Promise.all(deleteSnippetPromises).then(() => {
+      deleteSingleSong(songId).then(resolve);
+    });
+  }).catch((error) => reject(error));
+});
+
+export { viewSongDetails, deleteSongSnippets, viewSnippetDetails };
