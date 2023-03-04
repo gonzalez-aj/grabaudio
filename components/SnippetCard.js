e17e0e0f233e4188f2cf05910455a2a290a6e0a4
Angie Gonzalez <angiem.go@gmail.com>
2023-03-04 15:20:05 -0600

    functioning cards on dom

--- components/SnippetCard.js
+++ components/SnippetCard.js
@@ -1,7 +1,62 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import Image from 'next/image';
+import songluetransparent from '../images/songluetransparent.png';
+import { deleteSingleSnippet } from '../api/snippetData';
+import { useAuth } from '../utils/context/authContext';
 
-export default function SnippetCard() {
+function SnippetCard({ snippetObj, onUpdate }) {
+  const { user } = useAuth();
+  const deleteSnippet = () => {
+    if (window.confirm(`You sure you wanna delte this lil snippets ${snippetObj.name}?`)) {
+      deleteSingleSnippet(snippetObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
   return (
-    <div>SnippetCard</div>
+    <>
+      <Card style={{ width: '20rem', margin: '10px' }}>
+        <Image variant="top" src={songluetransparent} alt={snippetObj.title} />
+        <Card.Body>
+          <Card.Title>{snippetObj.title}</Card.Title>
+
+          <audio controls>
+            <source src={snippetObj.audio_url} />
+            <track kind="captions" />
+          </audio>
+
+          <p className="card-text bold"> Description: {snippetObj.description} <br /> Key: {snippetObj.keyOf} <br /> BPM: {snippetObj.bpm} </p>
+          <Link href={`/snippet/${snippetObj.firebaseKey}`} passHref>
+            <Button variant="outline-dark" className="m-2">view</Button>
+          </Link>
+          <Link href={`/snippet/edit/${snippetObj.firebaseKey}`} passHref>
+            {snippetObj.uid === user.uid ? (<Button variant="outline-dark" className="m-2">edit</Button>) : '' }
+          </Link>
+
+          {snippetObj.uid === user.uid ? (<Button variant="outline-dark" className="m-2" onClick={deleteSnippet}>delete</Button>) : ''}
+
+        </Card.Body>
+      </Card>
+    </>
   );
 }
+
+SnippetCard.propTypes = {
+  snippetObj: PropTypes.shape({
+    audio_url: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    bpm: PropTypes.string,
+    name: PropTypes.string,
+    isPublic: PropTypes.bool,
+    favorite: PropTypes.bool,
+    keyOf: PropTypes.string,
+    firebaseKey: PropTypes.string,
+    uid: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default SnippetCard;
