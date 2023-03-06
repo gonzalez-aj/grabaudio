281a7cb1289d8179f45e405f018a2ce2e1079eec
Angie Gonzalez <angiem.go@gmail.com>
2023-03-06 15:02:34 -0600

    only updates title and description

--- pages/snippet/edit/[firebaseKey].js
+++ pages/snippet/edit/[firebaseKey].js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { getSingleSnippet } from '../../../api/snippetData';
+import SnippetForm from '../../../components/forms/SnippetForm';
 
-export default function UpdateSnippet() {
-  return (
-    <div>UpdateSnippet</div>
-  );
+export default function EditSnippet() {
+  const [editSnippet, setEditSnippet] = useState({});
+  const router = useRouter();
+  const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    getSingleSnippet(firebaseKey).then(setEditSnippet);
+  }, [firebaseKey]);
+
+  return (<SnippetForm obj={editSnippet} />);
 }
